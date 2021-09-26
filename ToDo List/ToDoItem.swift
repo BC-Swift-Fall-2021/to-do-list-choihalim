17b22147a7a60a79cd717625df8aa8c21aeb8a62
Halim Choi <choizk@bc.edu>
2021-09-25 22:56:50 -0400

    5.6 Structs

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by 최하림 on 9/25/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
